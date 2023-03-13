ccb6d8a390dc73ebc0389a98a15a10ece3292a92
colli <collinkovac01@gmail.com>
2023-03-13 17:56:59 -0500

    Second output test

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,17 @@ import java.util.concurrent.Semaphore;
 import java.util.Random;
 
 public class Main {
+
+    //creating the lock array for object accessing
+    static Semaphore[] lock;
+    public static void lockMaker(int objects){
+        lock = new Semaphore[objects];
+        for(int i = 0; i < lock.length; i++){
+            lock[i] = new Semaphore(1);
+        }
+    }
+
+
     public static void main(String[] args) {
         // Get number of domains N
         Random random = new Random();
@@ -11,6 +22,11 @@ public class Main {
         int M = 3+random.nextInt(6);
         // Create Access Matrix of size N*(M+N)
         String[][] AM = new String[N][M+N];
+
+        // calling the lockmaker function to create the lock
+        // array and initialize all permits to 1.
+        lockMaker(M);
+
         // Populate Access Matrix
 
         // Create domain threads
