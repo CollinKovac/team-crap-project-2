aed9cce9c2d03eb1eee283fa481daa07f341115f
paul <lordbabypanda@gmail.com>
2023-03-18 17:38:13 -0500

    somewhat finish CL, but need to test

--- src/CapabilityListsForDomains.java
+++ src/CapabilityListsForDomains.java
@@ -1,4 +1,4 @@
-/*import java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Random;
 import java.util.concurrent.locks.Lock;
@@ -32,75 +32,35 @@ public class CapabilityListsForDomains {
         for(int i = 0; i < N; i++){
             LinkedList<String> list = new LinkedList<String>();
             for(int j = 0; j < N + M; j++){
-                if(i < N + M){
-                    readOrWriter = random.nextInt(4);
+                if(j < M){ // each object does R | W | R/W - I'm unsure if this is correct
+                    readOrWriter = random.nextInt(3);
                     if(readOrWriter == 0)
                         list.add("D" + (j + ": R"));
                     else if(readOrWriter == 1)
                         list.add("D" + (j) + ": W");
                     else if(readOrWriter == 2)
                         list.add("D" + (j) + ": R/W");
-                    else{ // Domain switch access
-                        domainSwitch = random.nextInt(2);
-                        if(domainSwitch == 0 && i - M != j);
+                    else{
+                        System.out.println("Index out of bounds, line 44 in CL for domains");
                     }
                 }
-            }
-        }
-
-    }
-
-
-
-
-
-    /*
-    * public static void AccessList() {
-        // Get number of domains N
-        Random random = new Random();
-        int N = 3 + random.nextInt(5);
-        // Get number of objects M
-        int M = 3 + random.nextInt(5);
-
-        // calling the lockmaker function to create the lock
-        // array and initialize all permits to 1.
-        lockMaker(M);
-
-        // Create Access List (arraylist of linked lists for each object/domain)
-        List<Object> AL = new ArrayList<>(0);
-        // Populate Access List
-        int RorW;
-        int dSwitch;
-        for (int i = 0; i < M + N; i++) { // For each object/domain...
-            LinkedList<String> list = new LinkedList<String>(); // Create a linked list
-            for (int j = 0; j < N; j++) { // For each domain...
-                if (i < M) { // Object R/W access
-                    RorW = random.nextInt(4);
-                    if (RorW == 0) list.add("D" + (j + 1) + ": R");
-                    else if (RorW == 1) list.add("D" + (j + 1) + ": W");
-                    else if (RorW == 2) list.add("D" + (j + 1) + ": R/W");
-                } else { // Domain switch access
-                    dSwitch = random.nextInt(2);
-                    if (dSwitch == 0 && i - M != j) list.add("D" + (j + 1) + ": allow"); // Don't allow switching to self
+                else{ // Domain switch access
+                    domainSwitch = random.nextInt(2);
+                    if(domainSwitch == 0 && i - M != j)
+                        list.add("D" + j + ": allow");
                 }
             }
-            AL.add(list); // Add linked list to Access List
+            //add to each object as it goes down each domain
+            capabilityLists.add(list);
         }
-        // Print Access List
-        System.out.print(N + " domains\n" + M + " objects\nAccess List:");
-        for (int i = 0; i < M + N; i++) {
-            if (i < M) System.out.print("\nF" + (i + 1) + ": " + AL.get(i));
-            else System.out.print("\nD" + (i + 1 - M) + ": " + AL.get(i));
+        // Print Capability List
+        System.out.print(N + " domains \n" + M + "objects\n Capability List:");
+        for (int i = 0; i < N; i++){
+            if(i < N)
+                System.out.print("\nD" + (i - M) + ": " + capabilityLists.get(i));
+            else
+                System.out.print("\nF" + i + ": " + capabilityLists.get(i));
         }
-
-        // Create domain threads
-        Domain domain;
-        for(int i = 0; i < (int)Math.random() * (7-3) + 3; i++){
-            domain = new Domain(N,M, i);
-        }
+        System.out.println();
     }
-    *
-    * */
-
-
-//}
+}
